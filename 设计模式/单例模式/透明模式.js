49b63208af78e129179b068e1d5a0886daf845b8
Songnian <1127093059@qq.com>
2022-05-19 22:33:57 +0800

    稍微修改了透明模式

--- 设计模式/单例模式/透明模式.js
+++ 设计模式/单例模式/透明模式.js
@@ -1,12 +1,17 @@
+/* 所谓透明模式就是需求要用new而只能有一个
+   因此我们可以将第一个用new出来的对象引用保存到CreateSingleton.instance
+   之后直接返回就可以了
+*/
 class CreateSingleton {
     constructor(name) {
         /* 如果为空 */
         if (!CreateSingleton.instance) {
             this.name = name;
             CreateSingleton.instance = this;
-        } else {
-            return CreateSingleton.instance
         }
+        /* 直接返回第一个实例引用 */
+        return CreateSingleton.instance
+
     }
     getName() {
         return this.name
@@ -14,9 +19,11 @@ class CreateSingleton {
 }
 
 
-let Winner = new CreateSingleton('Winner');
-let Looser = new CreateSingleton('Looser');
+const Winner = new CreateSingleton('Winner');
+const Looser = new CreateSingleton('Looser');
 
 console.log(Winner === Looser); // true
 console.log(Winner.getName()); // 'Winner'
 console.log(Looser.getName());  // 'Winner'
+console.log(Winner);
+console.log(Looser);
